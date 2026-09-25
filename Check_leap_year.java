aa5c8ba377213aa5d4dc4feb34ebc3d7f1cc7b08
ALOK KUMAR <alokkumar1042005@gmail.com>
2026-09-26 00:49:12 +0530

    Refactor leap year check logic and improve output

--- Check_leap_year.java
+++ Check_leap_year.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class Check_leap_year {
-    public static void main(String [] args) {
+public class Check_leap_year{
+    public static void main( String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the year = ");
+        System.out.println("enter the year");
         int year = sc.nextInt();
-        if (year % 4 == 0) {
-            System.out.println( +(year) +   "  is a leap year");
-        } else if (year % 100 == 0) {
-            System.out.println( +(year) +   " is a leap year");
-        } else {
-            System.out.println(  +(year)  +   "  is not a leap year");
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            System.out.println( +(year) +"  is leap year " );
+        }
+        else{
+            System.out.println( +(year) + "  is not leap year");
         }
     }
-}
+}       
